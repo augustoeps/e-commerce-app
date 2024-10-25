ec24340ac6a88ecb406783907923f8d03f7e800c
Augusto <augustopaolone3@hotmail.com>
2024-10-25 11:14:34 +0100

    cuarto commit, añadi ya las funciones de traslado de imagen de ofertas y relevancias

--- app/src/main/java/com/example/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Recycler
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
 import com.example.myapplication.R
 import com.example.myapplication.SliderAdapter
 import com.example.myapplication.ViewModel.MainViewModel
@@ -16,7 +18,7 @@ import com.example.myapplication.databinding.ActivityIntroBinding
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.model.SliderModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     private val viewModel=MainViewModel()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +33,10 @@ class MainActivity : AppCompatActivity() {
     private fun initBanner() {
         binding.progressBarBanner.visibility= View.VISIBLE
         viewModel.banners.observe(this, Observer { items->
-
+            banners(items)
+            binding.progressBarBanner.visibility=View.GONE
         })
+        viewModel.loadBanners()
     }
     private fun banners(images:List<SliderModel>){
         binding.viewpagerSlider.adapter=SliderAdapter(images,binding.viewpagerSlider)
@@ -40,5 +44,14 @@ class MainActivity : AppCompatActivity() {
         binding.viewpagerSlider.clipChildren=false
         binding.viewpagerSlider.offscreenPageLimit=3
         binding.viewpagerSlider.getChildAt(0).overScrollMode=RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer=CompositePageTransformer().apply {
+            addTransformer(MarginPageTransformer(40))
+        }
+        binding.viewpagerSlider.setPageTransformer(compositePageTransformer)
+        if(images.size>1){
+            binding.dotIndicator.visibility=View.VISIBLE
+            binding.dotIndicator.attachTo(binding.viewpagerSlider)
+        }
     }
 }
