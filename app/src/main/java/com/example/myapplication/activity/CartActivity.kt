d6adf24318f993fccfc20a0f97637cb87c07d4a9
Augusto <augustopaolone3@hotmail.com>
2024-11-01 10:46:34 +0000

    añadimos backend del carrito de compra y finalizamos detalles de UI

--- app/src/main/java/com/example/myapplication/activity/CartActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/CartActivity.kt
@@ -1,17 +1,68 @@
 package com.example.myapplication.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.Adapter.CartAdapter
+import com.example.myapplication.Helper.ManagmentCart
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityCartBinding
+import com.example.project1762.Helper.ChangeNumberItemsListener
 
-class CartActivity : AppCompatActivity() {
+class CartActivity : BaseActivity() {
+    private lateinit var binding:ActivityCartBinding
+    private lateinit var managmentCart: ManagmentCart
+    private var tax:Double=0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_cart)
 
+        managmentCart= ManagmentCart(this)
+
+        setVariable()
+        initCartList()
+        calulateCart()
+    }
+
+    private fun initCartList() {
+        binding.viewCart.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.viewCart.adapter= CartAdapter(managmentCart.getListCart(),this,object :ChangeNumberItemsListener{
+            override fun onChanged() {
+                calulateCart()
+            }
+
+        })
+
+        with(binding){
+            emptyTxt.visibility=if (managmentCart.getListCart().isEmpty()) View.VISIBLE else View.GONE
+            scrollView2.visibility=if (managmentCart.getListCart().isEmpty())View.GONE else View.VISIBLE
+        }
+
+
+    }
+    private fun calulateCart(){
+        val percentTax=0.02
+        val delivery=10.0
+        tax=Math.round((managmentCart.getTotalFee()*percentTax)*100)/100.0
+        val total=Math.round((managmentCart.getTotalFee()+tax+delivery)*100)/100
+        val itemTotal=Math.round(managmentCart.getTotalFee()*100)/100
+
+        with(binding){
+            totalFeeTxt.text="€$itemTotal"
+            taxTxt.text="€$tax"
+            deliveryTxt.text="€$delivery"
+            totalTxt.text="€$total"
+
+        }
+    }
+    private fun setVariable() {
+        binding.backBtn.setOnClickListener{finish()}
     }
 }
